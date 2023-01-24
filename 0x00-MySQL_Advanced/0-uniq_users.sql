978ff5a701ec3ad972ddef518cb20c0dd700d891
girma <girmatarekegn.gi@gmail.com>
2023-01-24 08:44:21 +0300

    Add sqlbasics

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- create table name user
+-- With these attributes: id,email,name
+
+CREATE TABLE IF NOT EXISTS `users`(
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`email` VARCHAR(255) NOT NULL UNIQUE,
+`name` VARCHAR(255)
+);
